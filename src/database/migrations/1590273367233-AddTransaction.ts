5bb1e6a1fcec7ed64c1dd5137707901558d57b16
Jonathan Candido <jonathancsrr@gmail.com>
2020-05-25 19:06:15 -0300

    create transaction entity

---
+++ src/database/migrations/1590273367233-AddTransaction.ts
@@ -0,0 +1,42 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class AddTransaction1590273367233 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+    await queryRunner.createTable(
+      new Table({
+        name: 'transactions',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'value',
+            type: 'float',
+          },
+          {
+            name: 'category_id',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable('transactions');
+  }
+}
