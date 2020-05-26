f2c9bc6f6e7d34988f73faef914792ccafb8d879
Jonathan Candido <jonathancsrr@gmail.com>
2020-05-26 19:31:50 -0300

    add delete route for transaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,21 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const transactionExist = await transactionRepository.findOne({
+      where: { id },
+    });
+
+    if (!transactionExist) {
+      throw new AppError('Transaction not exists', 204);
+    }
+
+    transactionRepository.delete(transactionExist);
   }
 }
 
